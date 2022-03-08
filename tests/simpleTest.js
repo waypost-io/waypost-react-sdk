4cf26db004a7fab4caf55358746e28f04cff28ed
Julia Martin <juliadmartin720@gmail.com>
2022-03-07 22:10:57 -0800

    Added React Context Provider which contains the SDK client, added webpack

--- tests/simpleTest.js
+++ tests/simpleTest.js
@@ -1,11 +1,11 @@
-Config = require('./Config');
+const Config = require('../lib/Config.js');
 
 const config = new Config('12345', "http://localhost:5000", 1000).connect();
 const sdkClient = config.client;
 // sdkClient.addContext({ userId: "abcde" });
 
 const logStatus = () => {
-  if (sdkClient.evaluateFlag("Test Flag 1")) {
+  if (sdkClient.evaluateFlag("Test Flag 1", false)) {
     console.log("Flag is on");
   } else {
     console.log("Flag is off");
