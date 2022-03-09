2220b029bb1ab7c0d43647f3c719b12231eddf6a
Julia Martin <juliadmartin720@gmail.com>
2022-03-08 20:54:35 -0800

    Get it to work on initial render

--- tests/simpleTest.js
+++ tests/simpleTest.js
@@ -1,11 +1,15 @@
 const Config = require('../lib/Config.js');
 
-const config = new Config('12345', "http://localhost:5000", 1000).connect();
-const sdkClient = config.client;
+let sdkClient;
+(async () => {
+  sdkClient = await new Config('12345', "http://localhost:5000", 1000).connect();
+  // console.log(config);
+  // sdkClient = config.client;
+})();
 // sdkClient.addContext({ userId: "abcde" });
 
 const logStatus = () => {
-  if (sdkClient.evaluateFlag("Test Flag 1", false)) {
+  if (sdkClient.evaluateFlag("Test flag 1", false)) {
     console.log("Flag is on");
   } else {
     console.log("Flag is off");
--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,8 @@ module.exports = {
         loader: 'babel-loader',
        }
     ]
+  },
+  externals: {
+    'react': 'react'
   }
 };
